528a6b7743c9f50ab4b6e8e78694cc5d9eb2410b
John <vti_cnf@yeah.net>
2022-11-06 00:03:11 +0800

    格式调整

--- src/main/java/com/aliyun/odps/cupid/presto/OdpsConnector.java
+++ src/main/java/com/aliyun/odps/cupid/presto/OdpsConnector.java
@@ -28,70 +28,56 @@ import javax.inject.Inject;
 import static com.aliyun.odps.cupid.presto.OdpsTransactionHandle.INSTANCE;
 import static java.util.Objects.requireNonNull;
 
-public class OdpsConnector
-        implements Connector
-{
-    private static final Logger log = Logger.get(OdpsConnector.class);
+public class OdpsConnector implements Connector {
+	private static final Logger log = Logger.get(OdpsConnector.class);
 
-    private final LifeCycleManager lifeCycleManager;
-    private final OdpsMetadata metadata;
-    private final OdpsSplitManager splitManager;
-    private final OdpsRecordSetProvider recordSetProvider;
-    private final OdpsPageSinkProvider pageSinkProvider;
+	private final LifeCycleManager lifeCycleManager;
+	private final OdpsMetadata metadata;
+	private final OdpsSplitManager splitManager;
+	private final OdpsRecordSetProvider recordSetProvider;
+	private final OdpsPageSinkProvider pageSinkProvider;
 
-    @Inject
-    public OdpsConnector(
-            LifeCycleManager lifeCycleManager,
-            OdpsMetadata metadata,
-            OdpsSplitManager splitManager,
-            OdpsRecordSetProvider recordSetProvider,
-            OdpsPageSinkProvider pageSinkProvider)
-    {
-        this.lifeCycleManager = requireNonNull(lifeCycleManager, "lifeCycleManager is null");
-        this.metadata = requireNonNull(metadata, "metadata is null");
-        this.splitManager = requireNonNull(splitManager, "splitManager is null");
-        this.recordSetProvider = requireNonNull(recordSetProvider, "recordSetProvider is null");
-        this.pageSinkProvider = requireNonNull(pageSinkProvider, "pageSinkProvider is null");
-    }
+	@Inject
+	public OdpsConnector(LifeCycleManager lifeCycleManager, OdpsMetadata metadata, OdpsSplitManager splitManager,
+			OdpsRecordSetProvider recordSetProvider, OdpsPageSinkProvider pageSinkProvider) {
+		this.lifeCycleManager = requireNonNull(lifeCycleManager, "lifeCycleManager is null");
+		this.metadata = requireNonNull(metadata, "metadata is null");
+		this.splitManager = requireNonNull(splitManager, "splitManager is null");
+		this.recordSetProvider = requireNonNull(recordSetProvider, "recordSetProvider is null");
+		this.pageSinkProvider = requireNonNull(pageSinkProvider, "pageSinkProvider is null");
+	}
 
-    @Override
-    public ConnectorTransactionHandle beginTransaction(IsolationLevel isolationLevel, boolean readOnly)
-    {
-        return INSTANCE;
-    }
+	@Override
+	public ConnectorTransactionHandle beginTransaction(IsolationLevel isolationLevel, boolean readOnly) {
+		return INSTANCE;
+	}
 
-    @Override
-    public ConnectorMetadata getMetadata(ConnectorTransactionHandle transactionHandle)
-    {
-        return metadata;
-    }
+	@Override
+	public ConnectorMetadata getMetadata(ConnectorTransactionHandle transactionHandle) {
+		return metadata;
+	}
 
-    @Override
-    public ConnectorSplitManager getSplitManager()
-    {
-        return splitManager;
-    }
+	@Override
+	public ConnectorSplitManager getSplitManager() {
+		return splitManager;
+	}
 
-    @Override
-    public ConnectorRecordSetProvider getRecordSetProvider()
-    {
-        return recordSetProvider;
-    }
+	@Override
+	public ConnectorRecordSetProvider getRecordSetProvider() {
+		return recordSetProvider;
+	}
 
-    @Override
-    public ConnectorPageSinkProvider getPageSinkProvider()
-    {
-        return pageSinkProvider;
-    }
+	@Override
+	public ConnectorPageSinkProvider getPageSinkProvider() {
+		return pageSinkProvider;
+	}
 
-    @Override
-    public final void shutdown()
-    {
-        try {
-            lifeCycleManager.stop();
-        }
-        catch (Exception e) {
-            log.error(e, "Error shutting down connector");
-        }
-    }
+	@Override
+	public final void shutdown() {
+		try {
+			lifeCycleManager.stop();
+		} catch (Exception e) {
+			log.error(e, "Error shutting down connector");
+		}
+	}
 }
